0b7f78eb81602e50cedd9996da3c8f23f3d33b39
Webbreacher <webbreacher@gmail.com>
2015-10-05 10:58:28 -0400

    Fixed some entries in JSON that had 'url' instead of 'check_uri'. Making the python checker script.

--- web_accounts_list_checker.py
+++ web_accounts_list_checker.py
@@ -2,17 +2,29 @@
     Author : Micah Hoffman (@WebBreacher)
     Description : Takes each username from the web_accounts_list.json file and performs the lookup to see if the discovery determinator is still valid
 '''
-
+from pprint import pprint
 import requests
 import json
 import os
 import urllib
 
 # Main
-
 # Read in the JSON file
+with open('web_accounts_list.json') as data_file:    
+    data = json.load(data_file)
+print '[-] We have %s sites that we will check.' % len(data['sites'])
+#pprint(data)
+
 # for each JSON entry
+for site in data['sites'] :
+	#pprint (site)
+	
 	# Perform lookup
+	# Pull the first user from known_accounts and replace the {account} with it
+	url = site['check_uri'].replace("{account}", site['known_accounts'][0])
+	print '[-] Looking up %s' % url
+	# Make web request for that URL
+	
 	# Analyze if lookup was successful
 	# If yes, 
 		# Mark valid == True
@@ -30,3 +42,4 @@ import urllib
 			# If yes, then then site issues
 				# Mark valid == False
 				# Update comments to note bad discovery string
+'''
