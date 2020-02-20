44e5096cd91009361d9e61be3300c02a543cabff
WebBreacher <WebBreacher@users.noreply.github.com>
2020-02-20 12:05:34 -0500

    fixing sites.

--- web_accounts_list_checker.py
+++ web_accounts_list_checker.py
@@ -30,7 +30,11 @@ import codecs
 # Variables && Functions
 ###################
 # Set HTTP Header info.
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language' : 'en-US,en;q=0.5',
+            'Accept-Encoding' : 'gzip, deflate'
+          }
 
 # Parse command line input
 parser = argparse.ArgumentParser(description="This standalone script will look up a single username using the JSON file"
