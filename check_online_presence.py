066f963d9a849478d25749893db31655434208f9
WebBreacher <WebBreacher@users.noreply.github.com>
2020-07-16 15:55:47 -0400

    Updating useragent

--- check_online_presence.py
+++ check_online_presence.py
@@ -16,7 +16,7 @@ DEBUG_MODE = False
 COUNTER = collections.Counter()
 
 # Set HTTP Header info.
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0',
            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Language' : 'en-US,en;q=0.5',
            'Accept-Encoding' : 'gzip, deflate'
