af25ddb8f0b1d953a705859cda7e68c077968f5b
Dan Cardin <dcardin2007@gmail.com>
2022-11-10 11:26:21 -0500

    Fixes issue #583

--- whatsmyname/app/models/schemas/cli.py
+++ whatsmyname/app/models/schemas/cli.py
@@ -25,7 +25,7 @@ class CliOptionsSchema(BaseModel):
     max_redirects: int = 10
     random_validate: bool = False
     random_username: Optional[str] = None,
-    user_agent_platform: str = 'Chrome on Windows 1'
+    user_agent_id: int = 1
     user_agent: Optional[str] = None
     capture_errors: bool = False
     capture_error_directory: Optional[str] = None
